3e5d208a76b8308a40114f016c697c3e21e4eb08
rizkymaulana803 <46336552+rizkymaulana803@users.noreply.github.com>
2019-01-03 18:48:14 +0700

    Add files via upload

---
+++ RIZKY MAULANA-311810588 program  luas lingkaran.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+const float phi = 3.14;
+const char nama[] = "Doni Irawan";
+
+int main () {
+	printf("%f\n",phi);
+	float luas = 7 * 7 * phi;
+	printf("%f\n",luas);
+	printf("%s\n",nama);
+}
